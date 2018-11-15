1b59ecbb06a7aaab5bbd26a106f5e99c1300c656
SLCTECH\ddolan <quyon_danny@hotmail.com>
2018-11-15 10:37:05 -0500

    Added Utility

--- starter/Tower Defense Chapter 18 Starter/Assets/Scripts/Utility/LoopRotate.cs
+++ starter/Tower Defense Chapter 18 Starter/Assets/Scripts/Utility/LoopRotate.cs
@@ -4,13 +4,18 @@ using UnityEngine;
 
 public class LoopRotate : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    public Vector3 rotation;
+
+
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+
+        transform.Rotate(rotation * Time.deltaTime);
+
+    }
 }
